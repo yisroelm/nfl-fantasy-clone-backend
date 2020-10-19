4e953e68d884ca77a766608e34e07a9f53ce0974
Mahmoud Agamy <agamyv@gmail.com>
2020-10-19 15:36:34 -0400

    testing

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 const express = require("express");
 const app = express();
 app.use("/", (req, res) => res.send("<h1>Heyyy</h1>"));
-app.listen(3000, () => console.log("server running hi")
+app.listen(3000, () => console.log("server running......"));
