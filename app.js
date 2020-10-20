b1d31d7d8af36cb6725a01b85c95ef3ff94149a9
Mahmoud Agamy <agamyv@gmail.com>
2020-10-20 19:00:23 -0400

    testing

--- app.js
+++ app.js
@@ -2,7 +2,4 @@ import express from "express";
 const app = express();
 
 app.use("/", (req, res) => res.send("<h1>Heyyy</h1>"));
-<<<<<<< HEAD
 app.listen(3000, () => console.log("server running.... good"));
-=======
->>>>>>> 239a408be9d58e6849ab3b0ed2001b7a696a61f5
