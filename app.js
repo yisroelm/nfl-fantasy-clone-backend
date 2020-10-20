5f48b924999025638cd9a55b7bbd2b6e503c89fa
Mahmoud Agamy <agamyv@gmail.com>
2020-10-20 19:59:32 -0400

    created routes controllers and established the db connection

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
-import express from "express";
+const express = require("express");
+const userRoute = require("./app/routes/userRoute");
+
 const app = express();
 
+app.use("/api/user", userRoute);
 app.use("/", (req, res) => res.send("<h1>Heyyy</h1>"));
 app.listen(3000, () => console.log("server running.... good"));
