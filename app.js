5c7359e6ce30c9112540a10ce4e9952e41d4c379
Mahmoud Agamy <agamyv@gmail.com>
2020-10-20 18:34:27 -0400

    es6 support

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-const express = require("express");
+import express from "express";
 const app = express();
+
 app.use("/", (req, res) => res.send("<h1>Heyyy</h1>"));
+
 app.listen(3000, () => console.log("server running.... good"));
