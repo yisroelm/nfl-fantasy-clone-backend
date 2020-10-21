46f3ce22ab478856cffa23400881335cfec9e8e0
yisroel96 <yisroel96@gmail.com>
2020-10-21 10:30:15 -0400

    testing

--- app.js
+++ app.js
@@ -5,4 +5,4 @@ const app = express();
 
 app.use("/api/user", userRoute);
 app.use("/", (req, res) => res.send("<h1>Heyyy</h1>"));
-app.listen(3000, () => console.log("server running.... good"));
+app.listen(3000, () => console.log("server running... good"));
