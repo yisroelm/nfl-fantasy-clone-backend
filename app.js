0451229dfbd6c7bfc4ff1d541b3ff9b5ceb34b20
Mahmoud Agamy <agamyv@gmail.com>
2020-10-21 18:06:07 -0400

    creating team model route controller

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const userRoute = require("./app/routes/userRoute");
+const teamRoute = require("./app/routes/teamRoute");
 
 const app = express();
 
 app.use("/api/user", userRoute);
+app.use("/api/team", teamRoute);
 app.use("/", (req, res) => res.send("<h1>Heyyy</h1>"));
 app.listen(3000, () => console.log("server running.... good"));
