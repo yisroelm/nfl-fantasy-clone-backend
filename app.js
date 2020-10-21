87cb4188b12aa85884811dc6731dbf159bf0abe5
Mahmoud Agamy <agamyv@gmail.com>
2020-10-21 18:09:15 -0400

    creating player model route controller

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const userRoute = require("./app/routes/userRoute");
 const teamRoute = require("./app/routes/teamRoute");
+const playerRoute = require("./app/routes/playerRoute");
 
 const app = express();
 
 app.use("/api/user", userRoute);
 app.use("/api/team", teamRoute);
+app.use("/api/player", playerRoute);
 app.use("/", (req, res) => res.send("<h1>Heyyy</h1>"));
 app.listen(3000, () => console.log("server running.... good"));
